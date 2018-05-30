6252db52758e23145dca584ca28313cf0f1edd6d
Leon boris <leonboripgs0818@gmail.com>
2018-05-30 17:41:26 +1000

    new file

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Taxon></Taxon>
+  </div>
+</template>
+
+<script>
+
+import Taxon from '@/components/Taxon'
+
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {Taxon},
+  methods: {
+    
+  }
+}
+
+</script>
---
+++ src/components/Taxon.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+      <v-tabs color="cyan" dark slider-color="yellow">
+        <v-tab v-for="(tab, key) in $store.state.tab_list" :key="key" ripple v-on:dblclick="editTab(key)">
+          {{tab}}
+        </v-tab>
+        <v-spacer></v-spacer>
+        <v-btn color="info" v-on:click="editTab(-1)">New Tab</v-btn>
+      </v-tabs>
+      
+      <v-dialog v-model="editDlg" max-width="500px">
+        <v-card>
+          <v-card-title>
+            {{this.modalName}}
+          </v-card-title>
+          <v-card-text>
+            <v-text-field v-model="new_name" placeholder="Input Taxon Name"></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+              <v-spacer></v-spacer>
+            <v-btn color="primary" flat @click.stop="saveName()">Save</v-btn>
+            <v-btn color="primary" flat @click.stop="editDlg = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Taxon',
+    data () {
+        return {
+            editDlg: false,
+            newDlg: false,
+            modalName: '',
+            new_name: '',
+            current_index: ''
+        }
+    },
+    mounted () {
+    },
+    methods: {
+        editTab (key) {
+            if(key != -1) {
+                this.new_name = this.$store.state.tab_list[key];
+                this.current_index = key;
+                this.modalName = "Change Tab Name";
+                this.editDlg = true;
+            } else {
+                this.new_name = '';
+                this.current_index = key;
+                this.modalName = "New Tab Name";
+                this.editDlg = true;
+            }
+        },
+        saveName () {
+            if (this.current_index == -1) {
+                this.$store.state.tab_list.push(this.new_name);
+
+            } else {
+                this.$store.state.tab_list[this.current_index] = this.new_name;
+            }
+            this.editDlg = false;
+
+        }
+    }
+}
+
+</script>
