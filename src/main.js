b20d2ce7e84569ed7e9c73297251029c6bd42c19
DESKTOP-R24\asharp <bciar@yandex.com>
2018-05-31 23:43:47 -0400

    Move Firebase App

--- src/main.js
+++ src/main.js
@@ -26,12 +26,12 @@ new Vue({
   template: '<App/>',
   created (){
   	firebase.initializeApp({
-	  	apiKey: 'AIzaSyDkjPgg0gJZv3H317Oi7PG0IJ7BL79S3lE',
-	    authDomain: 'fir-vue-a9e68.firebaseapp.com',
-	    databaseURL: 'https://fir-vue-a9e68.firebaseio.com',
-	    projectId: 'fir-vue-a9e68',
-	    storageBucket: '',
-	    messagingSenderId: '571313380191'
+      apiKey: "AIzaSyAvOjVb9-SwCFiWGDr1L9_PGz1Ei-1FVDc",
+      authDomain: "author2018-53022.firebaseapp.com",
+      databaseURL: "https://author2018-53022.firebaseio.com",
+      projectId: "author2018-53022",
+      storageBucket: "author2018-53022.appspot.com",
+      messagingSenderId: "515142910133"
   	})
   }
 })
